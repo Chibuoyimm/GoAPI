493e28a2e5fc4de568dfab58b15e90a3c8494225
Chibuoyim <onuigwechibuoyim@gmail.com>
2022-09-11 19:01:26 +0100

    Added a 'Get One'

--- main.go
+++ main.go
@@ -36,10 +36,24 @@ func deleteMovie(w http.ResponseWriter, r *http.Request){
 	for index, item := range movies {
 		if item.ID == params["id"] {
 			movies = append(movies[:index], movies[index+1:]...)  // this is the way you delete from a slice in Go; you basically join slices of the original slice
-			break
+			json.NewEncoder(w).Encode(movies)
+			return
 		}
 	}
 }
+
+func getMovie(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range movies {
+		if item.ID == params["id"]{
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
+
+
 
 func main(){
 	r := mux.NewRouter()
